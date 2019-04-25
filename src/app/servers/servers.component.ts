4b80cfb6fe3920fc65659075f57cfb754a808949
Mayuresh Srivastava <Mayuresh.Srivastava@rsystems.com>
2019-04-25 12:23:25 +0530

    Data binding and directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,21 +5,35 @@ import { Component, OnInit } from '@angular/core';
   //selector: '.app-servers',
   selector: 'app-servers',
   //template: `
-  //	<app-server></app-server>
-  //	<app-server></app-server>`,
+  //  <app-server></app-server>
+  //  <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-	allowNewServer = false;
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created.';
+  serverName = 'testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'TestServer2'];
 
   constructor() {
-  	setTimeout( () => {
-  		this.allowNewServer = true;
-  	},2000)
+    setTimeout( () => {
+      this.allowNewServer = true;
+    },2000)
   }
 
   ngOnInit() {
+  }
+
+  onServerCreate() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'A new server ' + this.serverName + ' has been created.'
+  }
+
+  onUpateServerName(event:any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
